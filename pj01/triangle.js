49ec85b381c9f3c1e2aebd413eb2316782cbf411
Tiago Teles <tiago.sequeira.teles@gmail.com>
2020-10-15 00:23:06 +0100

    Added checkbox menu, switch statement still not functional

--- pj01/triangle.js
+++ pj01/triangle.js
@@ -7,8 +7,6 @@ function resize(gl) {
 
 window.addEventListener("load", () => {
 
-    let timeScale = 1;
-
     let canvas = document.getElementById("gl-canvas");
     let gl = WebGLUtils.setupWebGL(canvas);
     if (!gl) {
@@ -16,12 +14,12 @@ window.addEventListener("load", () => {
     }
     resize(gl);
 
-    let buttons = document.getElementsByClassName("option");
+    let buttons = document.getElementsByClassName("toggle");
 
     for (b of buttons) {
         b.addEventListener("click", (elem) => {
             elem.target.classList.toggle("active");
-            toggleWave(elem.target.dataset.opt);
+            toggleButton(elem.target.dataset.opt);
         });
     }
 
@@ -244,9 +242,15 @@ window.addEventListener("load", () => {
         }
     }
 
-    function toggleWave(id) {
+    function toggleButton(id) {
         switch (id) {
-            case "sin":
+            case "C4":
+                break;
+            case "MC4":
+                break;
+            case "F4":
+                break;
+            case "merge":
                 break;
         }
     }
@@ -255,23 +259,27 @@ window.addEventListener("load", () => {
         let volts = [0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0, 200.0, 500.0];
         ev.target.parentElement.firstElementChild.innerText = "Y Scale [" + volts[ev.target.value] + "V]";
         yScale = 1 / volts[ev.target.value] / 4;
-    });
+	});
 
-    document.getElementById("freq1-slider").addEventListener("input", (ev) => {
-        let volts = [0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0, 200.0, 500.0];
-        ev.target.parentElement.firstElementChild.innerText = "Y Scale [" + volts[ev.target.value] + "V]";
-        yScale = 1 / volts[ev.target.value] / 4;
-    });
+	// function changeFreq(ev){
+	// 	const freqs = [["C3", 130.81], ["C#3/Db3", 138.59], ["D3", 146.83], ["D#3/Eb3", 155.56], ["E3", 164.81],
+	// 		["F3", 174.61], ["F#3/Gb3", 185.00], ["G3", 196.00], ["G#3/Ab3", 207.65], ["A3", 220.00],
+	// 		["A#3/Bb3", 233.08], ["B3", 246.94], ["C4", 261.63], ["C#4/Db4", 277.18], ["D4", 293.66],
+	// 		["D#4/Eb4", 311.13], ["E4", 329.63], ["F4", 349.23], ["F#4/Gb4", 369.99], ["G4", 392.00],
+	// 		["G#4/Ab4", 415.30], ["A4", 440.00], ["A#4/Bb4", 466.16], ["B4", 493.88], ["C5", 523.25]];
 
-    document.getElementById("freq2-slider").addEventListener("input", (ev) => {
-        let volts = [0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0, 200.0, 500.0];
-        ev.target.parentElement.firstElementChild.innerText = "Y Scale [" + volts[ev.target.value] + "V]";
-        yScale = 1 / volts[ev.target.value] / 4;
-    });
+        // ev.target.
+	// 		parentElement.firstElementChild.
+	// 		innerText = freqs[ev.target.value][0];
+
+
+	// }
 
     document.getElementById("x-slider").addEventListener("input", (ev) => {
-        let seconds = ["0.0001", "0.0002", "0.0005", "0.001", "0.002", "0.005", "0.01", "0.02", "0.05", "0.1", "0.2", "0.5", "1", "2", "5", "10"];
-        let secN = [0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10];
+        let seconds = ["0.0001", "0.0002", "0.0005", "0.001", "0.002", "0.005",
+			"0.01", "0.02", "0.05", "0.1", "0.2", "0.5", "1", "2", "5", "10"];
+        let secN = [0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02,
+			0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10];
         ev.target.parentElement.firstElementChild.innerText = "X Scale [" + seconds[ev.target.value] + "s]";
         xScale = secN[ev.target.value];
 
